baf4e47e6323e5b6cc27548a0734d98bd32b1137
piash-at-stml <maacpiash@outlook.com>
2018-11-17 20:44:56 +0600

    Started with WinForms

---
+++ WindowsFAHP.GUI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+using WindowsFAHP.GUI.Views;
+
+namespace WindowsFAHP.GUI
+{
+    internal class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.Run(new MainForm());
+        }
+    }
+}
